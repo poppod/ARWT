19d62b03fc628902b9708b3add6c1971034bc0c6
poppod <pop.podmenyou@gmail.com>
2021-08-09 15:45:48 +0700

    update new build

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ImgTrackTest.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/docs.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
